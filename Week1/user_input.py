43d0a3b75de29dc1cce893cf799f896a91067a3e
WanjuguMacharia <lauramacharia117@gmail.com>
2024-10-31 12:15:41 +0300

    my first python code

---
+++ Week1/user_input.py
@@ -0,0 +1,5 @@
+name = input('What is your name?')
+age = input('What is your age?')
+location = input('Where do you live?')
+
+print(f'Hello {name}. You are {age} years old and live in {location}')
